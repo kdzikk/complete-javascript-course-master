40e190ca5bdbf1d5ae74306ecf0d0f0f68b53c3e
Kamil Dzik <dzik-kamil@wp.pl>
2023-02-14 21:31:34 +0100

    scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const calcAge = birthYear => {
+  const age = 2037 - birthYear;
+
+  function prinAge() {
+    const output = `You are ${age} years old, born in ${birthYear}`;
+    console.log(output);
+  }
+
+
+  }
+
+  prinAge();
+  return age;
+};
+
+const firstName = 'Kamil';
+calcAge(1997);
